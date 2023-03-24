5ca7f581105223b96e9bd707b2fc7893f858f2ac
Marvin Briones <933584@cognizant.com>
2023-03-24 17:56:12 +0800

    added session9 Assignment

---
+++ EmployeeManagement/EmployeeManagement/Services/MessageBus/MessageBus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Services.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private readonly IBus _bus;
+
+        public void SendMessage(int employeeId, int newJobLevel)
+        {
+            _bus.Send("Type: EMPLOYEE JOB LEVEL CHANGED; " +
+                $"Id: {employeeId}; " +
+                $"NewJobLevel: {newJobLevel}");
+        }
+    }
+}
